2dbfcbc243d8e3dda1556ab3c28678452552f4a9
Alexander Weitz <ajveits@gmail.com>
2022-10-07 19:46:52 +0900

    renamed filename and added script to creatre anki deck

---
+++ download_images.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+from subprocess import call
+import urllib.request
+import time
+import random
+
+rc = call("./scrap_page.sh")
+
+f = open("bollards_temp3.txt", "r")
+bollards = f.read()  # read content of file into str
+f.close()  # close file
+
+bollard_list = bollards.split('\n')
+
+i = 0
+next_i = 0
+countries = {}
+country = None
+while i < len(bollard_list):
+    if bollard_list[i].startswith("Sources"):
+        if not bollard_list[i+1].startswith("Sources"):
+            country = bollard_list[i+1].strip() 
+        key_name = country
+        suffix = 1
+        while f"{key_name}_{suffix}" in countries:
+            suffix += 1
+        key_name = f"{key_name}_{suffix}" 
+        countries[key_name] = bollard_list[i] 
+    i += 1
+
+opener = urllib.request.build_opener()
+opener.addheaders = [
+    ('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)'),
+    ('Accept-Language','en-us')
+    ]
+urllib.request.install_opener(opener)
+
+for key, value in countries.items():
+    filename = f"bollards/{key}.jpg"
+    urllib.request.urlretrieve(f"https://geohints.com/{value}",filename)
+    print(filename)
+    time.sleep(1/random.randint(1, 9))
--- prepare_anki.py
+++ prepare_anki.py
@@ -1,43 +1,29 @@
-#!/bin/python3
+import genanki
 
-from subprocess import call
-import urllib.request
-import time
-import random
+my_model = genanki.Model(
+  1607392319,
+  'Simple Model',
+  fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Question}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+    },
+  ])
 
-rc = call("./scrap_page.sh")
+my_note = genanki.Note(
+  model=my_model,
+  fields=['Capital of Argentina', 'Buenos Aires'])
 
-f = open("bollards_temp3.txt", "r")
-bollards = f.read()  # read content of file into str
-f.close()  # close file
+my_deck = genanki.Deck(
+  2059400110,
+  'Country Capitals')
 
-bollard_list = bollards.split('\n')
+my_deck.add_note(my_note)
 
-i = 0
-next_i = 0
-countries = {}
-country = None
-while i < len(bollard_list):
-    if bollard_list[i].startswith("Sources"):
-        if not bollard_list[i+1].startswith("Sources"):
-            country = bollard_list[i+1].strip() 
-        key_name = country
-        suffix = 1
-        while f"{key_name}_{suffix}" in countries:
-            suffix += 1
-        key_name = f"{key_name}_{suffix}" 
-        countries[key_name] = bollard_list[i] 
-    i += 1
 
-opener = urllib.request.build_opener()
-opener.addheaders = [
-    ('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)'),
-    ('Accept-Language','en-us')
-    ]
-urllib.request.install_opener(opener)
-
-for key, value in countries.items():
-    filename = f"bollards/{key}.jpg"
-    urllib.request.urlretrieve(f"https://geohints.com/{value}",filename)
-    print(filename)
-    time.sleep(1/random.randint(0, 9))
+genanki.Package(my_deck).write_to_file('output.apkg')
